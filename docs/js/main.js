c1886f6dde5c6d67ee1cbfc083a165023353c89e
ytyaru <yttry0@gmail.com>
2022-06-02 20:56:04 +0900

    Created a repository

---
+++ docs/js/main.js
@@ -0,0 +1,34 @@
+window.addEventListener('DOMContentLoaded', async (event) => {
+    console.debug('DOMContentLoaded!!');
+    /*
+    document.getElementById('toot-button').addEventListener('click', (event) => {
+        document.getElementById('toot-dialog').showModal();
+        const status = document.getElementById('status');
+        if ('init' in status.dataset) { status.innerHTML = status.dataset.init }
+        status.innerHTML += '<br>' + location.href
+        status.focus();
+        setCaretStart(status)
+        document.getElementById('status').dispatchEvent(new Event('input'))
+    });
+    document.getElementById('status').addEventListener('input', (event) => {
+        const LIMIT = 500
+        const remaining = LIMIT - event.target.innerText.length
+        console.log(remaining )
+        document.getElementById('status-remaining').textContent = remaining;
+        console.log(event.target.innerText)
+    });
+    function setCaretStart(target) {
+        //status.setSelectionRange(0, 0);
+        var range = document.createRange()
+        var sel = window.getSelection()
+        range.setStart(target.childNodes[0], 0)
+        range.collapse(true)
+        sel.removeAllRanges()
+        sel.addRange(range)
+    }
+    */
+});
+window.addEventListener('beforeunload', (event) => {
+    console.debug('beforeunload!!');
+});
+
---
+++ docs/js/toot-dialog.js
@@ -0,0 +1,293 @@
+class TootDialog extends HTMLElement {
+    constructor() {
+        super();
+        this.domain = []
+        this.status = 'いいね！'
+        this.imgSrc = null
+        this.imgSize = '64'
+        this.title = 'トゥートする'
+        this.okMsg = 'トゥートしました！'
+        this.ngMsg = 'キャンセルしました。'
+    }
+    static get observedAttributes() {
+        return ['domain', 'status', 'img-src', 'img-size', 'title', 'ok-msg', 'ng-msg'];
+    }
+    async connectedCallback() {
+        console.log('=======================================')
+        //const shadow = this.attachShadow({ mode: 'closed' });
+        const shadow = this.attachShadow({ mode: 'open' }); // マウスイベント登録に必要だった。CSS的にはclosedにしたいのに。l
+
+        const button = await this.#makeButton()
+        const dialog = await this.#makeDialog()
+        shadow.innerHTML = `<style>${this.#makeCss()}</style>${button.outerHTML}${dialog.outerHTML}` 
+
+        console.debug(button.innerHTML)
+        //shadow.innerHTML = `<style>${this.#cssBase()}${this.#cssButton()}${this.#cssAnimation()}${this.#cssFocsAnimation()}</style>${button.outerHTML}` 
+        // pointer系 = mouse系 + touch系 + pen系
+        this.shadowRoot.querySelector('img').addEventListener('pointerdown', (e)=>{ e.target.classList.add('jump'); }, false);
+        //this.shadowRoot.querySelector('img').addEventListener('pointerover', (e)=>{ e.target.classList.add('flip'); }, false);
+        //this.shadowRoot.querySelector('img').addEventListener('mouseover', (e)=>{ e.target.classList.add('flip'); }, false);
+        this.shadowRoot.querySelector('img').addEventListener('animationend', (e)=>{ e.target.classList.remove('jump'); e.target.classList.remove('flip'); }, false);
+        this.#addListenerEvent(shadow)
+    }
+    #makeCss() { return `${this.#cssBase()}${this.#cssButton()}${this.#cssAnimation()}${this.#cssFocsAnimation()}` }
+    //#cssBase() { return `img{cursor:pointer; text-align:center; vertical-align:middle; user-select:none;}` }
+    #cssBase() { return `
+#toot-dialog {
+    top: 2.5%;
+    left: 2.5%;
+    padding: 0.25em;
+    margin: 0;
+    width: 90%;
+    height: 80%;
+    border: 4mm ridge rgba(211, 220, 50, .6);
+}
+#toot-dialog #status {
+    padding: 0.25em;
+    margin-bottom: 0.5em;
+    border: thick solid green;
+    border-radius: 0.5em;
+    outline: none;
+    background-color: #DDDDDD;
+}
+#toot-dialog #status:focus {
+    padding: 0.25em;
+    border: thick solid green;
+    border-radius: 0.5em;
+    background-color: #FFFFFF;
+    outline: none;
+}
+#toot-dialog textarea {
+    width: 200px;
+    height: 200px;
+}
+#toot-dialog a:focus img {
+    border: thick double #32a1ce;
+}
+#toot-button {
+    width: auto;
+    padding: 0;
+    margin: 0;
+    background: none;
+    border: 0;
+    font-size: 0;
+    line-height: 0;
+    overflow: visible;
+    cursor: pointer;
+}
+`
+    }
+    #cssButton() { return `
+button {
+    width: auto;
+    padding: 0;
+    margin: 0;
+    background: none;
+    border: 0;
+    font-size: 0;
+    line-height: 0;
+    overflow: visible;
+    cursor: pointer;
+}`
+}
+    #cssAnimation() { return `
+@keyframes jump {
+  from {
+    position:relative;
+    bottom:0;
+    transform: rotateY(0);
+  }
+  45% {
+    position:relative;
+    bottom: ${this.imgSize*2}px;
+  }
+  55% {
+    position:relative;
+    bottom: ${this.imgSize*2}px;
+  }
+  to {
+    position:relative;
+    bottom: 0;
+    transform: rotateY(720deg);
+  }
+}
+.jump {
+  transform-origin: 50% 50%;
+  animation: jump .5s linear alternate;
+}
+@keyframes flip {
+  from {
+    transform: rotateY(0);
+  }
+  to {
+    transform: rotateY(180deg);
+  }
+}
+.flip {
+  transform-origin: 50% 50%;
+  animation: flip .20s linear alternate;
+}`; }
+    #cssFocsAnimation() { return `
+button {
+  width: ${this.imgSize}px;
+  height: ${this.imgSize}px;
+}
+button:focus {
+  transform-origin: 50% 50%;
+  animation: flip .20s linear alternate;
+}
+button, button img {
+  width: ${this.imgSize}px;
+  height: ${this.imgSize}px;
+  z-index: 1;
+}
+button:focus, button:focus img {
+  width: ${this.imgSize * 1.5}px;
+  height: ${this.imgSize * 1.5}px;
+  z-index: 9999;
+  vertical-align:bottom;
+}
+`
+    }
+    attributeChangedCallback(property, oldValue, newValue) {
+        if (oldValue === newValue) { return; }
+        if ('domain' === property) { this.domain = newValue.split(' ')}
+        else if ('img-src' === property) { this.imgSrc = newValue}
+        else if ('img-size' === property) { this.imgSize = newValue}
+        else if ('ok-msg' === property) { this.okMsg = newValue}
+        else if ('ng-msg' === property) { this.ngMsg = newValue}
+        else { this[property] = newValue; }
+    }
+    async #makeButton() {
+        const button = await this.#makeButtonElement()
+        const img = this.#makeButtonImg()
+        button.appendChild(img)
+        return button
+    }
+    #makeButtonElement() {
+        const button = document.createElement('button')
+        //a.setAttribute('title', this.title)
+        button.setAttribute('id', 'toot-button')
+        button.setAttribute('title', this.title)
+        return button
+    }
+    #makeButtonImg() {
+        const img = document.createElement('img')
+        const size = this.#parseImgSize()
+        const [width, height] = this.#parseImgSize()
+        img.setAttribute('width', `${width}`)
+        img.setAttribute('height', `${height}`)
+        img.setAttribute('src', `${this.#getImgSrc()}`)
+        //img.classList.add('flip'); // 初回アニメーション用
+        return img
+    }
+    #getImgWidth() { return parseInt( (0 <= this.imgSize.indexOf('x')) ? this.imgSize.split('x')[0] : this.imgSize) }
+    #getImgHeight() { return parseInt( (0 <= this.imgSize.indexOf('x')) ? this.imgSize.split('x')[1] : this.imgSize) }
+    #parseImgSize() {
+        if (0 <= this.imgSize.indexOf('x')) { return this.imgSize.split('x').map(v=>(parseInt(v)) ? parseInt(v) : 64) }
+        else { return (parseInt(this.imgSize)) ? [parseInt(this.imgSize), parseInt(this.imgSize)] : [64, 64] }
+    }
+    #getImgSrc() {
+        return `./asset/image/mastodon_mascot.svg`
+        /*
+        console.debug(this.domain, this.imgSize)
+        if (this.imgSrc) { return this.imgSrc }
+        //return `http://www.google.com/s2/favicons?domain=${this.domain}`
+        if (this.domain) { return `https://t0.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://${this.domain}&size=${this.imgSize}` }
+        return `./asset/image/mastodon_mascot.svg`
+        */
+        /*
+        if (this.imgSrc) { return this.imgSrc }
+        if (this.img) {
+            const num = parseInt(this.img)
+            if (isNaN(num)) {
+                const key = this.icon.getKey(this.img, this.imgSize)
+                return (this.icon.Base64.has(key)) ? this.icon.Base64.get(key) : this.icon.Default }
+            else {
+                if (this.icon.Base64.size <= num) { return this.icon.Default }
+                if (num < this.icon.Base64.size) { return [...this.icon.Base64.values()][num] }
+                return this.icon.get(this.img, this.imgSize)
+            }
+        }
+        return this.icon.Default
+        */
+    }
+    #makeDialog() {
+        const dialog = document.createElement('dialog')
+        dialog.setAttribute('id', 'toot-dialog')
+        const form = document.createElement('form')
+        form.setAttribute('method', 'dialog')
+
+        const status = document.createElement('div')
+        status.setAttribute('id', 'status')
+        status.setAttribute('contenteditable', 'true')
+        const remaining = document.createElement('span')
+        remaining.setAttribute('id', 'status-remaining')
+
+        // ボタン生成
+        const buttons = []
+        for (const domain of this.domain) {
+            buttons.push(`<toot-button domain="${domain}"></toot-button>`)
+        }
+        buttons.push(`<toot-button></toot-button>`)
+        buttons.push(`<button id="toot-dialog-close"><span style="font-size:${this.imgSize}px;">❌</span></button>`)
+
+        form.appendChild(status)
+        form.appendChild(remaining)
+        form.innerHTML += buttons.join('')
+        dialog.appendChild(form)
+        return dialog
+    }
+    #addListenerEvent(shadow) { // トゥートボタンを押したときの動作を実装する
+        console.log(this.shadowRoot)
+        this.shadowRoot.getElementById('toot-button').addEventListener('click', (event) => {
+            this.shadowRoot.getElementById('toot-dialog').showModal();
+            const status = this.shadowRoot.getElementById('status');
+            status.innerText = this.status
+            status.innerHTML += '<br>' + location.href
+            /*
+            if ('init' in status.dataset) { status.innerHTML = status.dataset.init }
+            if ('init' in .dataset) { status.innerHTML = status.dataset.init }
+            status.innerHTML += '<br>' + location.href
+            */
+            status.focus();
+            this.#setCaretStart(status)
+            this.shadowRoot.getElementById('status').dispatchEvent(new Event('input'))
+        });
+        console.debug('--------------------------')
+        console.debug(this.shadowRoot.getElementById('status'))
+        this.shadowRoot.getElementById('status').addEventListener('input', (event) => {
+            const LIMIT = 500
+            const remaining = LIMIT - event.target.innerText.length
+            console.debug(remaining)
+            this.shadowRoot.getElementById('status-remaining').textContent = remaining;
+            console.debug(event.target.innerText)
+        });
+        //for (const button of this.shadowRoot.getElementsByName('toot-button')) {
+        for (const button of this.shadowRoot.querySelectorAll(`toot-button`)) {
+            button.addEventListener('click', (event) => {
+                console.debug('トゥートボタンを押した。値を渡す。')
+                event.target.setAttribute('status', this.shadowRoot.getElementById('status').innerText)
+            });
+        }
+        /*
+        this.shadowRoot.getElementByName('toot-button').addEventListener('click', (event) => {
+            console.debug('トゥートボタンを押した。値を渡す。')
+            event.target.setAttribute('status', this.shadowRoot.getElementById('status'))
+        });
+        */
+    }
+    #setCaretStart(target) {
+        //status.setSelectionRange(0, 0);
+        var range = document.createRange()
+        var sel = window.getSelection()
+        range.setStart(target.childNodes[0], 0)
+        range.collapse(true)
+        sel.removeAllRanges()
+        sel.addRange(range)
+    }
+}
+window.addEventListener('DOMContentLoaded', (event) => {
+    customElements.define('toot-dialog', TootDialog);
+});
+
